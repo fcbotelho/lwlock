2cd213803e4381a6e5cad26981db38655129710f
Fabiano C. Botelho <fcbotelho@gmail.com>
2014-03-22 18:02:07 -0700

    Adding lw_util.h

---
+++ src/lw_util.h
@@ -0,0 +1,44 @@
+#ifndef __LW_UTIL_H__
+#define __LW_UTIL_H__
+#include <stdarg.h>
+/**
+ *  Emit a print string and store it in the specified buffer.
+ *   
+ *  @param buf (i) Pointer to buffer to store string.
+ *  @param len (i) Size of string buffer.
+ *  @param pos (i) Pointer to string position value
+ *                (Should be initialized to 0 on first call)
+ *  @param fmt (i) the input parameter specifying the printf-like
+ *                 format specification.
+ *  @param var-args (i) optional parameters corresponding to the
+ *                      specified format.
+ *  
+ *
+ *  @description
+ *  
+ *  This routine is similar to snprintf(), but oriented towards emiting
+ *  a sequence of strings and storing them one after another in a single
+ *  buffer.  This routine tracks the position in the buffer and returns
+ *  -1 if the buffer length is exceeded.  
+ */
+static inline int
+lw_printbuf(char *buf, size_t len, size_t *pos, const char *fmt, ...)
+{
+    va_list args;
+    int ret;
+
+    lw_verify(*pos < len);
+
+    va_start(args, fmt);
+    ret = vsnprintf(&buf[*pos], (len - *pos), fmt, args);
+    va_end(args);
+
+    if (ret < 0 || (size_t)ret >= (len - *pos)) {
+        *pos += (len - *pos - 1);
+        return -1;
+    } else {
+        *pos += ret;
+        return 0;
+    }
+}
+#endif
